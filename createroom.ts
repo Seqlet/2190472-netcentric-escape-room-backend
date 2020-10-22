0834e5c17aa35d12f285edeb68163ce1a209e4cf
tongerring <summa.tongerring@gmail.com>
2020-10-23 01:25:21 +0700

    commit message

--- createroom.ts
+++ createroom.ts
@@ -1,34 +1,45 @@
-import random, { randomID } from "./random";
+import  {random, randomID, randomPos } from "./random";
 import { nanoid } from "nanoid";
-import { Player, HatType, PlayerType, Game } from "./Interfaces/index";
-io.on("connect, onConnect");
-function onConnect(socket) {
-  console.log("a client connected");
-  //create player object
-  socket.on("given name", oncreateGame => {
-    console.log("message: " + oncreateGame);
+import { Player, HatType, PlayerType, Game, Event } from "./Interfaces/index";
+import { isObject } from "util";
+import { Server } from 'socket.io';
+import gameArray  from './game'
+
+function createGame(playerName: string, io: Server) {
+  let randomRoomcode = randomID()
+  while(gameArray.find(game => game.roomCode === randomRoomcode)) {
+    randomRoomcode = randomID()
+  } 
+  
+  let game: Game = {
+    exitPosition: { x: random(5), y: random(5) },
+    obstaclePositions: [],
+    timer: 10,
+    winner: null,
+    currentPlayer: random(2),
+    roomCode: randomRoomcode,
+    players: []
+  };
+
     let player1: Player = {
       id: randomID(),
-      name: oncreateGame,
+      name: playerName,
       hatType: HatType.OLD,
       playerType: random(2),
-      position: { x: random(5), y: random(5) },
+      position: randomPos(game),
       victory: 0
     };
-    let game: Game = {
-      exitPosition: { x: random(5), y: random(5) },
-      obstaclePositions: [
-        { x: random(5), y: random(5) },
-        { x: random(5), y: random(5) },
-        { x: random(5), y: random(5) },
-        { x: random(5), y: random(5) },
-        { x: random(5), y: random(5) },
-      ],
-      timer: 10,
-      winner: null,
-      currentPlayer: random(2),
-      roomCode: randomID(),
-      players: [player1],
-    };
-  });
+
+    game.players.push(player1)
+
+    for (let i = 0; i < 5; i++) {
+      game.obstaclePositions.push(randomPos(game));
+    }
+
+    gameArray.push(game)
+
+    io.emit(Event.CREATE_GAME, player1)
 }
+
+
+export default createGame
--- server.ts
+++ server.ts
@@ -4,9 +4,10 @@ const app = express();
 import http from "http";
 const server = http.createServer(app);
 const PORT = process.env.PORT || 3000;
-import socketio from "socket.io";
+import socketio, {Socket} from "socket.io";
 import prejoinRoom from "./prejoinroom";
 import joinRoom from "./joinroom";
+import createGame from "./createroom";
 const io = socketio(server);
 
 server.listen(PORT, () => console.log(`Server running on port ${PORT}`));
@@ -15,12 +16,15 @@ app.get("/", (req, res) => {
   res.end("TGR");
 });
 
-io.on("connection", function (socket) {
+io.on("connection", function (socket: Socket) {
   console.log("user connected");
   socket.on(Event.PREJOIN_ROOM, function (roomCode: string) {
     prejoinRoom(roomCode, io);
   });
   socket.on(Event.JOIN_ROOM, function (data: JoinRoom) {
     joinRoom(data, io);
+  });
+  socket.on(Event.CREATE_GAME, function (playerName: string) {
+    createGame(playerName, io);
   });
 });

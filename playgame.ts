d644b710220d613c50a071a205b07d3431dda3b6
new5558 <new17353@gmail.com>
2020-11-02 12:21:07 +0700

    fix: spectator mode error

--- playgame.ts
+++ playgame.ts
@@ -1,55 +1,57 @@
 import { Player, Event, PlayerType, Position } from "./Interfaces/index";
 import { Server } from "socket.io";
 import gameArray, { timers } from "./game";
-import { findGame } from "./findgame";
 import { comparePosition, isNearbyPosition } from "./check";
 
 function playGame(updatedPlayer: Player, io: Server) {
-  const updatedPlayerId = updatedPlayer.id;
-  const gameIndex = gameArray.findIndex((game) =>
-    game.players.find((player) => updatedPlayer.id === player.id)
-  );
-  const currentGame = gameArray[gameIndex];
-  let playerIndex = currentGame?.players.findIndex(
-    (player) => player.id === updatedPlayerId
-  );
-  const nearbyPosition = isNearbyPosition(
-    updatedPlayer.position as Position,
-    currentGame.players[playerIndex].position as Position
-  );
-  if (
-    playerIndex !== undefined &&
-    nearbyPosition &&
-    !currentGame.obstaclePositions.find((obsPosition) =>
-      comparePosition(updatedPlayer.position, obsPosition.x, obsPosition.y)
-    ) &&
-    currentGame?.currentPlayer === updatedPlayer.playerType
-  ) {
-    currentGame!.players[playerIndex as number] = updatedPlayer;
-    currentGame.currentPlayer = 1 - currentGame.currentPlayer;
+  if (updatedPlayer.playerType !== PlayerType.SPECTATOR) {
+    const updatedPlayerId = updatedPlayer.id;
+    const gameIndex = gameArray.findIndex((game) =>
+      game.players.find((player) => updatedPlayer.id === player.id)
+    );
+    const currentGame = gameArray[gameIndex];
+    let playerIndex = currentGame?.players.findIndex(
+      (player) => player.id === updatedPlayerId
+    );
+    const nearbyPosition = isNearbyPosition(
+      updatedPlayer.position as Position,
+      currentGame.players[playerIndex].position as Position
+    );
     if (
-      comparePosition(
-        currentGame!.players[1 - playerIndex].position,
-        updatedPlayer.position!.x,
-        updatedPlayer.position!.y
-      )
+      playerIndex !== undefined &&
+      nearbyPosition &&
+      !currentGame.obstaclePositions.find((obsPosition) =>
+        comparePosition(updatedPlayer.position, obsPosition.x, obsPosition.y)
+      ) &&
+      currentGame?.currentPlayer === updatedPlayer.playerType
     ) {
-      clearInterval(timers[gameIndex] as NodeJS.Timeout);
-      currentGame.winner = PlayerType.WARDER;
+      currentGame!.players[playerIndex as number] = updatedPlayer;
+      currentGame.currentPlayer = 1 - currentGame.currentPlayer;
+      if (
+        comparePosition(
+          currentGame!.players[1 - playerIndex].position,
+          updatedPlayer.position!.x,
+          updatedPlayer.position!.y
+        )
+      ) {
+        clearInterval(timers[gameIndex] as NodeJS.Timeout);
+        currentGame.winner = PlayerType.WARDER;
+      }
+      if (
+        updatedPlayer.playerType === PlayerType.PRISONER &&
+        comparePosition(
+          currentGame.exitPosition,
+          updatedPlayer.position!.x,
+          updatedPlayer.position!.y
+        )
+      ) {
+        clearInterval(timers[gameIndex] as NodeJS.Timeout);
+        currentGame.winner = PlayerType.PRISONER;
+      }
+      currentGame.timer = 10;
+      io.to(currentGame?.roomCode || "").emit(Event.PLAY_GAME, currentGame);
     }
-    if (
-      updatedPlayer.playerType === PlayerType.PRISONER &&
-      comparePosition(
-        currentGame.exitPosition,
-        updatedPlayer.position!.x,
-        updatedPlayer.position!.y
-      )
-    ) {
-      clearInterval(timers[gameIndex] as NodeJS.Timeout);
-      currentGame.winner = PlayerType.PRISONER;
-    }
-    currentGame.timer = 10;
-    io.to(currentGame?.roomCode || "").emit(Event.PLAY_GAME, currentGame);
   }
 }
+
 export default playGame;
